65ef2f511b218ff18cef47447adb5465673ba9c1
Lakshy Yadav <lakshyyadav2007@gmail.com>
2025-06-27 16:01:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,11 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
+          crossOrigin="anonymous"
         ></link>
       </head>
       <body className={cn("min-h-screen bg-background font-body antialiased")}>
